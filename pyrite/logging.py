675f8c8138081b1942855ecf2030417486171788
SwayKh <khatrisumit712@gmail.com>
2026-08-25 17:50:40 +0200

    feat: Added Targets

--- pyrite/logging.py
+++ pyrite/logging.py
@@ -1,17 +1,20 @@
+import sys
+
 class Logger:
     ERROR = 1
     WARN  = 2
     INFO  = 3
 
-    def __init__(self, level):
+    def __init__(self, level, file=sys.stdout):
         self.level = level
+        self.file = file
 
     def info(self, msg):
         if self.level > self.WARN:
-            print(f"[INFO] {msg}")
+            print(f"[INFO] {msg}", file=self.file)
     def warn(self, msg):
         if self.level > self.ERROR:
-            print(f"[WARN] {msg}")
+            print(f"[WARN] {msg}", file=self.file)
     def error(self, msg):
-        print(f"[ERROR] {msg}")
+        print(f"[ERROR] {msg}", file=self.file)
     
